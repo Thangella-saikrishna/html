b23cee017ed4e89de7036a9fffd90cc52018f1d9
saikittu25 <saikrishnathangella@gmail.com>
2025-02-07 15:45:58 +0530

    event
    ev

--- java.js
+++ java.js
@@ -1,6 +1,20 @@
-let message ="Hello , world!";
-const PI = 3.14;
-function greet (name) {
-    return 'Hello , ${name}!';
-}
-greet("sai");
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
